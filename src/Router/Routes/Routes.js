cdc826cff98df75f6e93bcebaaa5bd3c4e032d6e
aminulhaque98 <aminulhaque419@gmail.com>
2022-12-23 03:39:55 +0530

    basic setup and header footer set on the page

---
+++ src/Router/Routes/Routes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../../Layout/Main';
+import Home from '../../Pages/Home/Home/Home';
+import NotFound from '../../Pages/NotFoundedPage/NotFound';
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+
+        ]
+    },
+    {
+        path: '*',
+        element: <NotFound></NotFound>
+    }
+])
+
+export default routes;
