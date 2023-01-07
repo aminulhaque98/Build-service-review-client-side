5bd2c34c2bc9b767d0a2302438e16a5f49847cb4
aminulhaque98 <aminulhaque419@gmail.com>
2023-01-08 01:23:24 +0530

    animation spin

--- src/Router/PrivateRoute/PrivateRoute.js
+++ src/Router/PrivateRoute/PrivateRoute.js
@@ -7,7 +7,9 @@ const PrivateRoute = ({ children }) => {
     const location = useLocation();
 
     if (loading) {
-        return <div className="radial-progress bg-primary text-primary-content border-4 border-primary" style={{ "--value": 70 }}>70%</div>
+        return <div className='text-center'>
+            <div className="animate-spin radial-progress bg-primary text-primary-content border-4 border-primary" style={{ "--value": 70 }}></div>
+        </div>
     }
 
     if (user && user?.uid) {
